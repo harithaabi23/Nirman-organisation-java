a6066b1ad1bd1cec76170edf8f160bb4d0f18cb5
harithaabi23 <126463228+harithaabi23@users.noreply.github.com>
2024-11-21 12:42:27 +0530

    Add files via upload
    day 2: story task

---
+++ StoryTask.java
@@ -0,0 +1,22 @@
+public class StoryTask{
+public static void main (String args[]){
+String name1 = "abi";
+String adjective1 = "fresh";
+String adjective2 = "wonderful";
+String noun1 = "adults";
+String noun2 = "animals";
+String verb1 = "groove";
+String noun3 = "beats";
+String noun4 = "adults";
+String adjective3 = "exicted";
+String name2 = "thahseen";
+String place1 = "canada";
+String noun5 = "softie";
+String number = "2026";
+String noun6 = "robots";
+
+
+String story = "This morning " + name1 + " woke up feeling " + adjective1 + ". 'It is going to be a " + adjective2 + " day!' Outside, a bunch of " + noun1+ "s were protesting to keep " + noun2 + " in stores. They began to" + verb1 + " to the rhythm of the " + noun3 + ", which made all the " + noun4 + "s very " + adjective3 + ". Concerned, " + name1 + " texted " + name2 + ", who flew " + name1 + " to " + place1 + " and dropped " +name1 + " in a puddle of frozen "+noun5 + ". " + name1 + " woke up in the year " + number + ", in a world where " + noun6 + "s ruled the world.";
+
+System.out.println(story);
+}}
