3eb2e60d076a8c842ca02827d86d7a5a45aa8ff3
harithaabi23 <126463228+harithaabi23@users.noreply.github.com>
2024-12-03 12:56:30 +0530

    day8

---
+++ dotEqualsEx.java
@@ -0,0 +1,14 @@
+package day8;
+
+public class dotEqualsEx {
+public static void main(String[] args) {
+	String name=" dead ";
+	String str = new String(" dead ");
+	
+	if(name.equals(str)) {
+		System.out.print("Surya weds meghna");
+	}else {
+		System.out.print("Surya weds ramya");
+	}
+}
+}
