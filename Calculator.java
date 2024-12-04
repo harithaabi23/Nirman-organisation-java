e266e648f66f934e560680d177ccd63c39988432
harithaabi23 <126463228+harithaabi23@users.noreply.github.com>
2024-12-04 11:57:02 +0530

    day9

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+package day9;
+
+public class Calculator {
+void add(int a, int b) {
+	System.out.println("addition: " +(a+b));
+}
+void sub(int a, int b) {
+	System.out.println("subtraction: " +(a-b));
+}
+
+int mul(int a, int b) {
+	return a*b;
+}
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+package day9;
+
+public class Main {
+	
+int a;
+int b ;
+	public static void main(String[] args) {
+	Calculator cal = new Calculator();
+	cal.add(20, 30);
+   
+    System.out.println(cal.mul(2,3 ));
+}}
