6c141fb264bc335f2b9d6f854c8cea7d2606d83e
harithaabi23 <126463228+harithaabi23@users.noreply.github.com>
2024-12-19 11:56:06 +0530

    day 18 wrap

---
+++ Wrap.java
@@ -0,0 +1,12 @@
+package day18;
+
+public class Wrap {
+public static void main(String[] args) {
+	int num = 10;
+	Integer autoBoxingInt = num;
+	System.out.println(autoBoxingInt);
+	
+	int unBoxing  = autoBoxingInt;
+	System.out.println(unBoxing );
+}
+}
